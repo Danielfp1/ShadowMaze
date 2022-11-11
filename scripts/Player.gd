67e2d4b396aae554b616b67276413fa224e60bf1
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-11-11 17:32:38 -0300

    Adicionar animação do Herói

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,15 +1,18 @@
 extends KinematicBody2D
 
+onready var collision: CollisionShape2D = get_node("AttackArea/Collision")
 onready var animation: AnimationPlayer = get_node("Animation")
 onready var sprite: Sprite = get_node("Sprite")
 
 var velocity: Vector2
 var can_die: bool = false
+var can_attack: bool = false
 
 export (int) var speed
 
 func _physics_process(_delta: float) -> void:
 	move()
+	attack()
 	verify_direction()
 	animate()
 	
@@ -23,9 +26,19 @@ func move() -> void:
 	velocity = direction_vector * speed
 	velocity = move_and_slide(velocity)
 
+
+func attack() -> void:
+	if Input.is_action_just_pressed("Attack") and not can_attack:
+		pass
+		can_attack = true
+
+
 func animate() -> void:
 	if can_die:
 		animation.play("Death")
+		set_physics_process(false)
+	elif can_attack:
+		animation.play("Attack")
 		set_physics_process(false)
 	elif velocity != Vector2.ZERO:
 		animation.play("Run")
@@ -35,8 +48,10 @@ func animate() -> void:
 func verify_direction() -> void:
 	if velocity.x > 0:
 		sprite.flip_h = false
+		collision.position = Vector2(22.5,-7.5)
 	elif velocity.x < 0:
 		sprite.flip_h = true
+		collision.position = Vector2(-22.5,-7.5)
 		
 func kill() -> void:
 	can_die = true
@@ -45,3 +60,6 @@ func kill() -> void:
 func on_animation_finished(anim_name):
 	if anim_name == "Death":
 		var _reload: bool = get_tree().reload_current_scene()
+	elif anim_name == "Attack":
+		can_attack = false
+		set_physics_process(true)
