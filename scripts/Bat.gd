264b0ea9463551148d4eb5fb5605cdadb9332a92
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-11-27 14:19:08 -0300

    Implementar sistema de HP em todos inimigos

--- scripts/Bat.gd
+++ scripts/Bat.gd
@@ -6,7 +6,18 @@ onready var sprite: Sprite = get_node("Sprite")
 var player_ref = null
 var velocity: Vector2
 
+#Flags
+var can_die: bool = false
+var can_attack: bool = false
+
 export (int) var speed
+
+#Vida
+export (int) var health = 4
+
+#Knockback
+export (bool) var receives_knockback: bool = true
+export (float) var knockback_modifier: float = 40
 
 func _physics_process(_delta: float) -> void:
 	move()
@@ -18,19 +29,26 @@ func move() ->void:
 		var distance: Vector2 = player_ref.global_position - global_position
 		var direction: Vector2 = distance.normalized()
 		var distance_length: float = distance.length()
-		if distance_length <= 16:
+		if distance_length <= 20:
 			velocity = Vector2.ZERO
-			player_ref.kill()
+			can_attack = true
 		else:
 			velocity = speed * direction
-		print(distance_length)
+			can_attack = false
 	else:
 		velocity = Vector2.ZERO
 	velocity = move_and_slide(velocity)
 
 func animate() -> void:
-	if velocity != Vector2.ZERO:
+	if can_die:
+		animation.play("Death")
+		set_physics_process(false)
+	elif velocity != Vector2.ZERO:
 		animation.play("Walk")
+	elif receives_knockback==false:
+		animation.play("Hit")
+	elif can_attack:
+		animation.play("Attack")
 	else:
 		animation.play("Idle")
 
@@ -48,3 +66,23 @@ func on_body_exited(body):
 	if body.is_in_group("player"):
 		player_ref = null
  
+func hit() -> void:
+	receive_knockback(player_ref.global_position)
+	health=health-1
+	if health<0:
+		kill()
+
+func kill() -> void:
+	can_die = true
+
+func receive_knockback(damage_source_pos: Vector2):
+	if receives_knockback:
+		var knockback_direction = damage_source_pos.direction_to(self.global_position)
+		var knockback = knockback_direction * knockback_modifier
+		global_position += knockback
+		
+
+func _on_Animation_animation_finished(anim_name):
+	if anim_name == "Death":
+		queue_free()
+
